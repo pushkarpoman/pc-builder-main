0ede22e4e37334a046c88748c25f248d2a08ddb0
pushkarpoman <pushkarpoman10@gmail.com>
2025-04-25 19:14:40 +0530

    Pc_Builder

--- src/layout/Menubar.jsx
+++ src/layout/Menubar.jsx
@@ -9,13 +9,13 @@ const Menubar = () => {
             CPU / Processor
             <ul className="hidden z-50 absolute top-10 bg-white border border-gray-300 rounded shadow group-hover:block border-t-4 border-t-secondary">
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 1</Link>
+                <Link href="#">Amd Ryzen</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 2</Link>
+                <Link href="#">Intel</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 3</Link>
+                <Link href="#">Other</Link>
               </li>
             </ul>
           </li>
@@ -23,13 +23,13 @@ const Menubar = () => {
             Motherboard
             <ul className="hidden z-50 absolute top-10 bg-white border border-gray-300 rounded shadow group-hover:block border-t-4 border-t-secondary">
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 1</Link>
+                <Link href="#">Msi </Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 2</Link>
+                <Link href="#">Asus</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 3</Link>
+                <Link href="#">Gigabyte</Link>
               </li>
             </ul>
           </li>
@@ -37,13 +37,13 @@ const Menubar = () => {
             Ram
             <ul className="hidden z-50 absolute top-10 bg-white border border-gray-300 rounded shadow group-hover:block border-t-4 border-t-secondary">
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 1</Link>
+                <Link href="#">Crosair</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 2</Link>
+                <Link href="#">HyperX</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 3</Link>
+                <Link href="#">Fury</Link>
               </li>
             </ul>
           </li>
@@ -51,13 +51,13 @@ const Menubar = () => {
             Power supply unit
             <ul className="hidden z-50 absolute top-10 bg-white border border-gray-300 rounded shadow group-hover:block border-t-4 border-t-secondary">
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 1</Link>
+                <Link href="#">Circle</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 2</Link>
+                <Link href="#">Antech</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 3</Link>
+                <Link href="#">Zebronics</Link>
               </li>
             </ul>
           </li>
@@ -65,13 +65,13 @@ const Menubar = () => {
             Storage device
             <ul className="hidden z-50 absolute top-10 bg-white border border-gray-300 rounded shadow group-hover:block border-t-4 border-t-secondary">
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 1</Link>
+                <Link href="#">HDD</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 2</Link>
+                <Link href="#">SDD</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 3</Link>
+                <Link href="#">M.2 nvme </Link>
               </li>
             </ul>
           </li>
@@ -79,13 +79,13 @@ const Menubar = () => {
             Monitor
             <ul className="hidden z-50 absolute top-10 bg-white border border-gray-300 rounded shadow group-hover:block border-t-4 border-t-secondary">
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 1</Link>
+                <Link href="#">Acer</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 2</Link>
+                <Link href="#">Alienware</Link>
               </li>
               <li className="px-4 py-2 hover:bg-gray-100">
-                <Link href="#">Option 3</Link>
+                <Link href="#">Zowie</Link>
               </li>
             </ul>
           </li>
