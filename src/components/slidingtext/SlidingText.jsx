a0cebb2ff3f69b09d4e408d1a287267fcb3a7102
pushkarpoman <pushkarpoman10@gmail.com>
2025-07-19 08:26:52 +0530

    Update SlidingText.jsx

--- src/components/slidingtext/SlidingText.jsx
+++ src/components/slidingtext/SlidingText.jsx
@@ -3,7 +3,7 @@ const SlidingText = () => {
     <div className="px-1 md:main-container">
       <div className="bg-white rounded-full flex items-center justify-center px-2 py-1 overflow-hidden">
         <div className="scrolling-text">
-          This website is build by Pushkar Poman & Ruchira Wakure @2025 All rights reserved.
+          This website is build by Pushkar Poman @2025 All rights reserved.
         </div>
       </div>
     </div>
